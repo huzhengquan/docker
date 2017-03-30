2b8343d996bb76b9ace67ef6e3d94ae9069d1566
huzhengquan <huzhengquan@gmail.com>
2017-03-30 10:19:38 +0800

    es

--- elasticsearch-ik/run.sh
+++ elasticsearch-ik/run.sh
@@ -1,6 +1,6 @@
 docker run \
   -p 9200:9200 -p 9300:9300 \
-  -v /Users/huzhengquan/work-data/esdata:/usr/share/elasticsearch/data \
+  -v /Volumes/work/rs/esdata:/usr/share/elasticsearch/data \
   --name elasticsearch \
   -d huzhengquan/elasticsearch-ik:5 
 
